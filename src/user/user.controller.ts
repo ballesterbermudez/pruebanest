c1fc9823508e7a779cb6feafb851b906bd971ee2
Federico Sierra <federico.sierradodo@cencosud.com>
2022-11-02 15:20:57 -0300

    add: create user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { Serialize } from '../interceptors/serialize.interceptor';
+import { CreateUserDto } from './dtos/create-user';
+import { UserDto } from './dtos/user.dto';
+import { UserService } from './user.service';
+
 
 @Controller('user')
-export class UserController {}
+@Serialize(UserDto)
+export class UserController {
+    constructor(private userService: UserService) {}
+
+    @Post()
+    async createUser(@Body() body: CreateUserDto){
+        const user = await this.userService.createUser(body.name, body.email, body.password);
+        return user;
+    }
+}
