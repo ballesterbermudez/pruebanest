38b8721607a2da1922bf60d881878a1148d721ea
ballester.moreira <ballester.moreira@cencosud.com>
2022-11-02 14:34:56 -0300

    primer commit

---
+++ src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { ProductModule } from './product/product.module';
+import { PictureModule } from './pictures/picture.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    UserModule,
+    ProductModule,
+    PictureModule,
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.prueba',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
